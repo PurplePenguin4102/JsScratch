53c14e2909db220a17270b77e1c15b72b0a0e7a3
Joey <joseph.h.ray@gmail.com>
2017-05-14 21:34:22 +1000

    I've done all sorts of nonsense

--- ModuleDemo.js
+++ ModuleDemo.js
@@ -10,18 +10,48 @@
         // $scope and $http are objects defined by angular. $scope tells us what's available for bindings
         // and http lets us make AJAX requests to remote resources
 
+        // a simple binding to anything called {{message}} in MainCtrl ng-controller block of the html
+        $scope.message = "Prepare for Angular Magic!!!"
+
         // -- define behaviours
-        var onResponse = function(resp) {
-            $scope.user = resp.data;
+        var onResponse1 = function(resp) {
+            $scope.user1 = resp.data;
+            $http.get($scope.user1.repos_url)
+                 .then(onRepos1, onError);
         };
+
+        var onResponse2 = function(resp) {
+            $scope.user2 = resp.data;
+            $http.get($scope.user2.repos_url)
+                 .then(onRepos2, onError);
+        };
+
+        var onRepos1 = function(resp) {
+            $scope.repos1 = resp.data;
+        }
+
+        var onRepos2 = function(resp) {
+            $scope.repos2 = resp.data;
+        }
 
         var onError = function(err) {
-            $scope.error = "No such user exists :(";
+            $scope.error = "No such data, we're fucked :(";
         };
 
-        // -- invoke our webservice, similar to HttpClient use .then callback instead of await/async pattern
-        $http.get("https://api.github.com/users/purplepenguin4102")
-             .then(onResponse, onError);
+        $scope.search1 = function(username) {
+            // -- invoke our webservice, similar to HttpClient use .then callback instead of await/async pattern
+            $http.get("https://api.github.com/users/" + username)
+                 .then(onResponse1, onError);
+        }
+
+        $scope.search2 = function(username) {
+            // -- invoke our webservice, similar to HttpClient use .then callback instead of await/async pattern
+            $http.get("https://api.github.com/users/" + username)
+                 .then(onResponse2, onError);
+        }
+
+        $scope.repo1SortOrder = "-stargazers_count"
+        $scope.repo2SortOrder = "-stargazers_count"
     };
 
     // after contoller definition, dump it in our module
